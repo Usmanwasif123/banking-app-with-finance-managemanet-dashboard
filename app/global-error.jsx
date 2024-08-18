acea9d13813007f75c20b36b3c828570a5b01af4
Usmanwasif123 <usmanwasif8@gmail.com>
2024-08-18 18:06:21 +0500

    Remove Sentry references from global-error.jsx

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -1,12 +1,12 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
+// import * as Sentry from "@sentry/nextjs"; // Comment out this line
 import { useEffect } from "react";
 
 export default function GlobalError({ error }) {
   useEffect(() => {
-    Sentry.captureException(error);
+    // Sentry.captureException(error); // Comment out this line
   }, [error]);
 
   return (
